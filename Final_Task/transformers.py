2c56631311ca62722e11f651820a1fcc4171947c
egor_pilat <Egor_Pilat@epam.com>
2021-06-17 20:29:32 +0300

    add Final_task_v1

---
+++ Final_Task/transformers.py
@@ -0,0 +1,18 @@
+#from collections import defaultdict as dd
+#from datetime import datetime
+#from copy import deepcopy
+#from datetime import datetime
+#import re
+
+
+class FeatureTransformer:
+
+    @staticmethod
+    def transform_date(date):
+        pass
+
+    @staticmethod
+    def price_to_float(price):
+        pass
+
+
