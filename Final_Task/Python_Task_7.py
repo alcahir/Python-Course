6eff0639c4b7c69a6278f7a0937797f1bc93a156
egor_pilat <Egor_Pilat@epam.com>
2021-06-17 20:08:07 +0300

    add Final_task

---
+++ Final_Task/Python_Task_7.py
@@ -0,0 +1,57 @@
+"""
+Task 1.1
+Implement a function which receives a string and replaces all '"' symbols
+with "'" and vise versa.
+"""
+
+#code ...
+
+
+"""
+#### Task 1.2
+Write a function that check whether a string is a palindrome or not. Usage of
+any reversing functions is prohibited. To check your implementation you can use
+strings from [here] (https://en.wikipedia.org/wiki/Palindrome#Famous_palindromes).
+"""
+
+#code ...
+
+
+"""
+#### Task 1.3
+Implement a function which works the same as `str.split` method
+(without using `str.split` itself, of course).
+"""
+
+# code ...
+
+"""
+#### Task 1.4
+Implement a function `get_digits(num: int) -> Tuple[int]` which returns a tuple
+of a given integer's digits.
+Example:
+python
+>>> split_by_index(87178291199)
+(8, 7, 1, 7, 8, 2, 9, 1, 1, 9, 9)
+"""
+
+#code ...
+
+
+"""
+#### Task 1.5
+Implement a function `foo(List[int]) -> List[int]` which, given a list of
+integers, return a new list such that each element at index `i` of the new list
+is the product of all the numbers in the original array except the one at `i`.
+Example:
+python
+>>> foo([1, 2, 3, 4, 5])
+[120, 60, 40, 30, 24]
+
+>>> foo([3, 2, 1])
+[2, 3, 6]
+"""
+
+#code ...
+
+
