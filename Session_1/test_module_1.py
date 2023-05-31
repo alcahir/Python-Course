9834faacd63eaf8a405f693e6405dc57a41f9eab
alcahir <Egor_Pilat@epam.com>
2023-06-01 01:42:34 +0300

    Updated session 1

--- Session_1/test_module_1.py
+++ Session_1/test_module_1.py
@@ -1,192 +1,112 @@
 import pytest
 
-from module_1 import *
-
-
-def test_task_1_correct_work():
-    numbers = []
-    for i in range(1, 1001):
-        if (i % 3 == 0) and (i % 5 == 0):
-            numbers.append(i)
-
-    output = task_1()
-
-    assert output == numbers, (
-        "Incorrect result"
-    )
+from .module_1 import *
 
 
 @pytest.mark.parametrize(
     'test_input, expected',
     [
-        pytest.param("12Axda1303dad", (6, 7), id='Example: 12Axda1303dad'),
-        pytest.param('0', (1, 0), id='Example: 0'),
-        pytest.param('abrakadabra', (0, 11), id='Example: abrakadabra'),
-        pytest.param('Data Engineering Lab is the best', (0, 27), id='Example: Data Engineering Lab is the best'),
-        pytest.param('???', (0, 0), id='Example: ???'),
-        pytest.param('', (0, 0), id='Example: '),
-        pytest.param('  a1', (1, 1), id='Example:   a1'),
-        pytest.param('11234', (5, 0), id='Example: 11234'),
+        pytest.param(([2, 7, 11, 15], 9), [2, 7], id='Example: sample=[2, 7, 11, 15], target=9'),
+        pytest.param(([3, 2, 4], 6), [2, 4], id='Example: sample=[3, 2, 4], target=6'),
+        pytest.param(([3, 3], 6), [3, 3], id='Example: sample=[3, 3], target=6'),
+        pytest.param(([2, 5, 5, 11], 10), [5, 5], id='Example: sample=[2, 5, 5, 11], target=10'),
+        pytest.param(([2], 2), [], id='Example: sample=[2], target=2'),
+        pytest.param(([2, 3], 5), [2, 3], id='Example: sample=[2, 3], target=5'),
+        pytest.param(([-1, 2, 12, 5, 111, -3], 9), [12, -3], id='Example: sample=[-1, 2, 12, 5, 111, -3], target=9'),
+        pytest.param(([1, 2, 3, 4, 5, 6, 7, 8, 9], 17), [8, 9], id='Example: sample=[1, 2, 3, 4, 5, 6, 7, 8, 9], target=17'),
+        pytest.param(([], 18), [], id='Example: sample=[], target=18'),
+        pytest.param(([-1, -1], 2), [], id='Example: sample=[-1, -1], target=2'),
     ],
 )
-def test_task_2_correct_work(test_input, expected):
-    output = task_2(test_input)
-    assert output == expected, (
-        "See tests!"
-    )
-
-
-@pytest.mark.parametrize(
-    'test_input_1, test_input_2, expected',
-    [
-        pytest.param(
-            ["red", "orange", "green", "blue", "white"],
-            ["black", "yellow", "green", "blue"],
-            (['white', 'orange', 'red'], ['black', 'yellow']),
-            id='Example: ["red", "orange", "green", "blue", "white"], ["black", "yellow", "green", "blue"]',
-        ),
-        pytest.param(
-            ["Mike", "John", "Ann"],
-            [12, "Egor", "Ann", "Jane"],
-            (["Mike", "John"], [12, "Egor", "Jane"]),
-            id='Example: ["Mike", "John", "Ann"], [12, "Egor", "Ann", "Jane"]',
-        ),
-        pytest.param(
-            [5, 5, 5],
-            [5, 5, 5],
-            ([], [],),
-            id='Example: [5, 5, 5], [5, 5, 5]',
-        ),
-        pytest.param(
-            [1, 5, 5],
-            [5, 1, 5],
-            ([], [],),
-            id='Example: [1, 5, 5], [5, 1, 5]',
-        ),
-        pytest.param(
-            [1, 5, 4],
-            [5, 1, 3],
-            ([4], [3],),
-            id='Example: [1, 5, 4], [5, 1, 3]',
-        ),
-        pytest.param(
-            [1, 5, 4, 6],
-            [5, 1, 4],
-            ([6], [],),
-            id='Example: [1, 5, 4, 6], [5, 1, 4]',
-        ),
-        pytest.param(
-            [1, 5, 4, 6],
-            [5, 1, 4],
-            ([6], [],),
-            id='Example: [1, 5, 4, 6], [5, 1, 4]',
-        ),
-        pytest.param(
-            [1, 2, 3],
-            [4, 5, 6],
-            ([1, 2, 3], [4, 5, 6],),
-            id='Example: [1, 2, 3], [4, 5, 6]',
-        ),
-    ],
-)
-def test_task_3_correct_work(test_input_1, test_input_2, expected):
-    output = task_3(test_input_1, test_input_2)
-    assert set(output[0]) == set(expected[0]), (
-        "Incorrect first difference"
-    )
-    assert set(output[1]) == set(expected[1]), (
-        "Incorrect second difference"
-    )
+def test_task_1(test_input, expected):
+    assert task_1(*test_input) == expected
 
 
 @pytest.mark.parametrize(
     'test_input, expected',
     [
-        pytest.param([11, 22, 33], 112233, id='Example: [11, 22, 33]'),
-        pytest.param([0, 0], 0, id='Example: [0, 0]'),
-        pytest.param([1], 1, id='Example: [1]'),
-        pytest.param([999, 0], 9990, id='Example: [999, 0]'),
-        pytest.param([999, 0, 10], 999010, id='Example: [999, 0, 10]'),
-        pytest.param([0, 0, 999, 0, 10, 0], 9990100, id='Example: [0, 0, 999, 0, 10, 0]'),
-        pytest.param([0, 0, 1, 0, 1], 101, id='Example: [0, 0, 999, 0, 10, 0]'),
+        pytest.param(12, 21, id='Example: sample=123'),
+        pytest.param(-123, -321, id='Example: sample=-123'),
+        pytest.param(120, 21, id='Example: sample=120'),
+        pytest.param(0, 0, id='Example: sample=0'),
+        pytest.param(555, 555, id='Example: sample=555'),
+        pytest.param(10, 1, id='Example: sample=1'),
+        pytest.param(-10, -1, id='Example: sample=-1'),
+        pytest.param(100, 1, id='Example: sample=100'),
     ],
 )
-def test_task_4_correct_work(test_input, expected):
-    output = task_4(test_input)
-    assert output == expected, (
-        "See tests!"
-    )
+def test_task_2(test_input, expected):
+    assert task_2(test_input) == expected
 
 
 @pytest.mark.parametrize(
     'test_input, expected',
     [
-        pytest.param([[11, 22, 33], [1, 2, 3]], [11, 22, 33], id='Example: [[11, 22, 33], [1,2,3]]'),
-        pytest.param([[0, 0, 1], [0, 0, 2]], [0, 0, 2], id='Example: [[0, 0, 1], [0,0,2]]'),
-        pytest.param([[0], [0]], [0], id='Example: [[0], [0]]'),
-        pytest.param([[1, 3, 2], [1, 2, 3]], [1, 3, 2], id='Example: [[1, 3, 2], [1, 2, 3]]'),
-        pytest.param([[10], [50], [40], [50], [25, 25]], [50], id='Example: [[10], [50], [40], [50], [25, 25]]'),
+        pytest.param([2, 1, 5, 2, 3, 3, 4], 2, id='Example: sample=[2, 1, 5, 2, 3, 3, 4]'),
+        pytest.param([2, 1, 5, 3, 3, 2, 4], 3, id='Example: sample=[2, 1, 5, 3, 3, 2, 4]'),
+        pytest.param([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], -1, id='Example: sample=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]'),
+        pytest.param([1, 1, 2, 3, 4, 5, 6, 7, 8, 9], 1, id='Example: sample=[1, 1, 2, 3, 4, 5, 6, 7, 8, 9]'),
+        pytest.param([1, 2, 3, 4, 5, 6, 7, 8, 9, 1], 1, id='Example: sample=[1, 2, 3, 4, 5, 6, 7, 8, 9, 1]'),
+        pytest.param([1, 2, 3, 4, 5, 6, 7, 8, 9, 9], 9, id='Example: sample=[1, 2, 3, 4, 5, 6, 7, 8, 9, 9]'),
+        pytest.param([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5], 5, id='Example: sample=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5]'),
+        pytest.param([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], -1, id='Example: sample=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -1]'),
+        pytest.param([], -1, id='Example: sample=[]'),
     ],
 )
-def test_task_5_correct_work(test_input, expected):
-    output = task_5(test_input)
-    assert output == expected, (
-        "See tests!"
-    )
+def test_task_3(test_input, expected):
+    assert task_3(test_input) == expected
 
 
 @pytest.mark.parametrize(
     'test_input, expected',
     [
-        pytest.param(111, 111, id='Example: 111'),
-        pytest.param(123, 321, id='Example: 123'),
-        pytest.param(-123, -321, id='Example: -123'),
-        pytest.param(2550, 552, id='Example: 2550'),
-        pytest.param(0, 0, id='Example: 0'),
+        pytest.param('III', 3, id='Example: sample=III'),
+        pytest.param('IV', 4, id='Example: sample=IV'),
+        pytest.param('IX', 9, id='Example: sample=IX'),
+        pytest.param('LVIII', 58, id='Example: sample=LVIII'),
+        pytest.param('MCMXCIV', 1994, id='Example: sample=MCMXCIV'),
+        pytest.param('MMXXI', 2021, id='Example: sample=MMXXI'),
+        pytest.param('MMXX', 2020, id='Example: sample=MMXX'),
+        pytest.param('MMXIX', 2019, id='Example: sample=MMXIX'),
+        pytest.param('MMXVIII', 2018, id='Example: sample=MMXVIII'),
+        pytest.param('MMXVII', 2017, id='Example: sample=MMXVII'),
+        pytest.param('MMXVI', 2016, id='Example: sample=MMXVI'),
+        pytest.param('MMXV', 2015, id='Example: sample=MMXV'),
+        pytest.param('MMXIV', 2014, id='Example: sample=MMXIV'),
+        pytest.param('MCMXCIV', 1994, id='Example: sample=MCMXCIV'),
+        pytest.param('MCMXCIII', 1993, id='Example: sample=MCMXCIII'),
+        pytest.param('MCMXCII', 1992, id='Example: sample=MCMXCII'),
+        pytest.param('C', 100, id='Example: sample=C'),
+        pytest.param('D', 500, id='Example: sample=D'),
+        pytest.param('M', 1000, id='Example: sample=M'),
+        pytest.param('L', 50, id='Example: sample=L'),
+        pytest.param('V', 5, id='Example: sample=V'),
+        pytest.param('I', 1, id='Example: sample=I'),
+        pytest.param('II', 2, id='Example: sample=II'),
+        pytest.param('VI', 6, id='Example: sample=VI'),
+        pytest.param('VII', 7, id='Example: sample=VII'),
+        pytest.param('VIII', 8, id='Example: sample=VIII'),
+        pytest.param('XI', 11, id='Example: sample=XI'),
     ],
 )
-def test_task_6_correct_work(test_input, expected):
-    output = task_6(test_input)
-    assert output == expected, (
-        "See tests!"
-    )
+def test_task_4(test_input, expected):
+    assert task_4(test_input) == expected
 
 
 @pytest.mark.parametrize(
     'test_input, expected',
     [
-        pytest.param("aba", "b", id='Example: aba'),
-        pytest.param("aaaccc", None, id='Example: aaaccc'),
-        pytest.param("aaacccb", "b", id='Example: aaacccb'),
-        pytest.param("", None, id='Example: '),
-        pytest.param("empty", "e", id='Example: empty'),
-        pytest.param("eempty", "m", id='Example: eempty'),
-        pytest.param("emptye", "m", id='Example: emptye'),
-        pytest.param("guccigu", "i", id='Example: guccigu'),
-        pytest.param("guccigu", "i", id='Example: guccigu'),
-        pytest.param("abcdfg", "a", id='Example: abcdfg'),
+        pytest.param([3, 4, 5, 1, 2], 1, id='Example: sample=[3, 4, 5, 1, 2]'),
+        pytest.param([4, 5, 6, 7, 0, 1, 2], 0, id='Example: sample=[4, 5, 6, 7, 0, 1, 2]'),
+        pytest.param([11, 13, 15, 17], 11, id='Example: sample=[11, 13, 15, 17]'),
+        pytest.param([1, 2, 3, 4, 5, 6, 7], 1, id='Example: sample=[1, 2, 3, 4, 5, 6, 7]'),
+        pytest.param([1, 2, 3, 4, 5, 6, 7, 0], 0, id='Example: sample=[1, 2, 3, 4, 5, 6, 7, 0]'),
+        pytest.param([1, 2, 3, 4, 5, 6, 7, 8, 9], 1, id='Example: sample=[1, 2, 3, 4, 5, 6, 7, 8, 9]'),
+        pytest.param([-1, 1, 0], -1, id='Example: sample=[-1, 1, 0]'),
+        pytest.param([1, 0], 0, id='Example: sample=[1, 0]'),
+        pytest.param([1], 1, id='Example: sample=[1]'),
+        pytest.param([-3, 5, 10, -2, -5, 50, 0], -5, id='Example: sample=[1, 2]'),
     ],
 )
-def test_task_7_correct_work(test_input, expected):
-    output = task_7(test_input)
-    assert output == expected, (
-        "See tests!"
-    )
-
-
-@pytest.mark.parametrize(
-    'test_input, expected',
-    [
-        pytest.param([0], [0], id='Example: [0]'),
-        pytest.param([1, 2, 3], [6, 3, 2], id='Example: [1,2,3]'),
-        pytest.param([1, 0, 0, 4], [0, 0, 0, 0], id='Example: [1, 0, 0, 4]'),
-        pytest.param([3, 0], [0, 3], id='Example: [3, 0]'),
-        pytest.param([3], [3], id='Example: [3]'),
-        pytest.param([5, 5, 5, 5], [125, 125, 125, 125], id='Example: [5, 5, 5, 5]'),
-    ],
-)
-def test_task_8_correct_work(test_input, expected):
-    output = task_8(test_input)
-    assert output == expected, (
-        "See tests!"
-    )
+def test_task_5(test_input, expected):
+    assert task_5(test_input) == expected
